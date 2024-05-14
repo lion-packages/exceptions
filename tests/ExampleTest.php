ee254a4e660b8a7fd2ea4918ba7b09118bcd9704
Sleon4 <sergioleon4004@hotmail.com>
2024-05-14 15:27:31 -0500

    first commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use Lion\Test\Test;
+
+class ExampleTest extends Test
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
